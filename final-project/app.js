911371d921ab50682fb6ff069f0661ed93083504
kongtuk2539 <thoty2539@gmail.com>
2023-09-05 04:08:36 +0700

    second-circle

--- final-project/app.js
+++ final-project/app.js
@@ -1,7 +1,7 @@
 const number = document.getElementById("number");
       const circle = document.getElementById("circle");
       let counter = 0;
-      let coin = 25;
+      let coin = 50;
       
       setInterval(() => {
         if (counter == coin) {
@@ -14,12 +14,12 @@ const number = document.getElementById("number");
 
       //440-440*% ex. 440-440*0.5 = 50%
       const coinPercent = coin / 100;
-      const valueStroke = 440 - 440 * coinPercent
+      const valueStroke = 780 - 780 * coinPercent
 
 
       const animation = circle.animate(
         [
-          { strokeDashoffset: 440 }, // Start with the circle fully hidden
+          { strokeDashoffset: 780 }, // Start with the circle fully hidden
           { strokeDashoffset: valueStroke }, // End with the circle fully visible
         ],
         {
